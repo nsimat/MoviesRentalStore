d8ded95a7859dd9340d92a4a4dc8b34c74e91f70
Michel Nsimba Matondo <nsimba_matondo@hotmail.com>
2018-11-09 01:51:43 +0100

    Ajoutez des fichiers projet.

---
+++ MoviesRentalStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesRentalStore.Startup))]
+namespace MoviesRentalStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
