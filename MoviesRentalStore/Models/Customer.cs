9e6e06f059c69dd981ac1ef3890a36236110a12c
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-05-04 02:27:37 +0200

    Adding MembershipType migration

--- MoviesRentalStore/Models/Customer.cs
+++ MoviesRentalStore/Models/Customer.cs
@@ -10,5 +10,7 @@ namespace MoviesRentalStore.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
