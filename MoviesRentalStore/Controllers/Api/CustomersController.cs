a6fedf1a408856a18c3c9ba61a2c244f74cd82c3
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-05-11 02:21:47 +0200

    Adding a web API to our MoviesRentalStore application

---
+++ MoviesRentalStore/Controllers/Api/CustomersController.cs
@@ -0,0 +1,83 @@
+﻿using MoviesRentalStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MoviesRentalStore.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET /api/Customers
+        public IEnumerable<Customer> GetAllCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // GET /api/Customers/id
+        public Customer GetCustomer(int id)
+        {
+            var customer =  _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                new HttpResponseException(HttpStatusCode.NotFound);
+
+            return customer;
+        }
+
+        // POST /api/Customers/id
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /api/Customers/id
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            customerInDB.Name = customer.Name;
+            customerInDB.BirthDate = customer.BirthDate;
+            customerInDB.MembershipTypeId = customer.MembershipTypeId;
+            customerInDB.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE /api/Customers/id
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDB == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDB);
+            _context.SaveChanges();
+        }
+    }
+}
