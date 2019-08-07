6525a11933fa3f9c3ae33edb645833989b9581b6
Michel Nsimba Matondo <nsimba_matondo@yahoo.fr>
2019-08-07 11:51:40 +0200

    Adding jquery script for auto-completion

--- MoviesRentalStore/Controllers/Api/NewRentalsController.cs
+++ MoviesRentalStore/Controllers/Api/NewRentalsController.cs
@@ -19,19 +19,6 @@ namespace MoviesRentalStore.Controllers.Api
             _context = new ApplicationDbContext();
         }
 
-
-        // GET: api/NewRental
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/NewRental/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/NewRental
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
@@ -61,16 +48,6 @@ namespace MoviesRentalStore.Controllers.Api
             _context.SaveChanges();
 
             return Ok();
-        }
-
-        // PUT: api/NewRental/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE: api/NewRental/5
-        public void Delete(int id)
-        {
         }
     }
 }
